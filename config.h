b89797efcee704df93b6397307fc812639395d4e
NomikuH8 <sofaco.checkin@gmail.com>
2022-04-09 20:48:51 -0300

    added my dmenu config

---
+++ config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"fira code:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#D9E0EE", "#1E1D2F" },
+	[SchemeSel] = { "#1E1E2E", "#F5C2E7" },
+	[SchemeOut] = { "#D9E0EE", "#96CDFB" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
